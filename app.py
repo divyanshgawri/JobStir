9002ecb72d255142be1832a44aeab79bb05bcb3b
divyanshgawri <dishugawri@gmail.com>
2025-08-16 02:25:47 +0530

    chore: force deploy

--- app.py
+++ app.py
@@ -3978,6 +3978,5 @@ def internal_error(error):
     return render_template('500.html'), 500
 
 # --- Main execution block ---
-if __name__ == '__main__':
-    
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == '__main__':    
+    app.run(debug=True, host='0.0.0.0', port=8000)
