063696eaef230602a3553783f4caf2459fafd08c
divyanshgawri <dishugawri@gmail.com>
2025-09-02 18:12:02 +0530

    ok

--- app.py
+++ app.py
@@ -2573,5 +2573,6 @@ def privacy_policy():
 def terms_of_service():
     return render_template('terms_of_service.html')
 # --- Main execution block ---
-if __name__ == '__main__':    
-    app.run(debug=True, host='0.0.0.0', port=8000)
+PORT = int(os.environ.get("PORT", 8000))
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=PORT)
